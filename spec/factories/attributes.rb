af780ca37c47c40f2442cfc5a2d7757176df61ac
Igor Belo <igorcoura@gmail.com>
2020-02-14 16:48:53 +0100

    add factory_bot

---
+++ spec/factories/attributes.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :entitlement, class: Hash do
+    expires_date { Time.now.utc.iso8601 }
+    purchase_date { Time.now.utc.iso8601 }
+    sequence(:product_identifier) { |n| "product-id-#{n}" }
+
+    factory :active_entitlement, class: Hash do
+      expires_date { (Time.now + 60).utc.iso8601 }
+    end
+
+    factory :expired_entitlement, class: Hash do
+      expires_date { (Time.now - 1).utc.iso8601 }
+    end
+  end
+end
