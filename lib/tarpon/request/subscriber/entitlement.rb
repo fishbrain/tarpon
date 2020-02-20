0eff9ade302ded2d3f602baa9bd8c07ed8bb2322
Igor Belo <igorcoura@gmail.com>
2020-02-20 10:44:36 +0100

    Uue nested class definitions instead of compact style

--- lib/tarpon/request/subscriber/entitlement.rb
+++ lib/tarpon/request/subscriber/entitlement.rb
@@ -2,29 +2,31 @@
 
 module Tarpon
   module Request
-    class Subscriber::Entitlement < Base
-      def initialize(subscriber_path:, entitlement_identifier:)
-        @subscriber_path = subscriber_path
-        @entitlement_identifier = entitlement_identifier
-      end
+    class Subscriber
+      class Entitlement < Base
+        def initialize(subscriber_path:, entitlement_identifier:)
+          @subscriber_path = subscriber_path
+          @entitlement_identifier = entitlement_identifier
+        end
 
-      def grant_promotional(duration:, start_time_ms: nil)
-        body = {
-          duration: duration,
-          start_time_ms: start_time_ms
-        }
+        def grant_promotional(duration:, start_time_ms: nil)
+          body = {
+            duration: duration,
+            start_time_ms: start_time_ms
+          }
 
-        perform(method: :post, path: "#{path}/promotional", key: :secret, body: body)
-      end
+          perform(method: :post, path: "#{path}/promotional", key: :secret, body: body)
+        end
 
-      def revoke_promotional
-        perform(method: :post, path: "#{path}/revoke_promotionals", key: :secret)
-      end
+        def revoke_promotional
+          perform(method: :post, path: "#{path}/revoke_promotionals", key: :secret)
+        end
 
-      private
+        private
 
-      def path
-        "#{@subscriber_path}/entitlements/#{@entitlement_identifier}"
+        def path
+          "#{@subscriber_path}/entitlements/#{@entitlement_identifier}"
+        end
       end
     end
   end
--- lib/tarpon/request/subscriber/subscription.rb
+++ lib/tarpon/request/subscriber/subscription.rb
@@ -2,22 +2,24 @@
 
 module Tarpon
   module Request
-    class Subscriber::Subscription < Base
-      def initialize(subscriber_path:, product_id:)
-        @subscriber_path = subscriber_path
-        @product_id = product_id
-      end
+    class Subscriber
+      class Subscription < Base
+        def initialize(subscriber_path:, product_id:)
+          @subscriber_path = subscriber_path
+          @product_id = product_id
+        end
 
-      def defer(expiry_time_ms:)
-        body = { expiry_time_ms: expiry_time_ms }
+        def defer(expiry_time_ms:)
+          body = { expiry_time_ms: expiry_time_ms }
 
-        perform(method: :post, path: "#{path}/defer", key: :secret, body: body)
-      end
+          perform(method: :post, path: "#{path}/defer", key: :secret, body: body)
+        end
 
-      private
+        private
 
-      def path
-        "#{@subscriber_path}/subscriptions/#{@product_id}"
+        def path
+          "#{@subscriber_path}/subscriptions/#{@product_id}"
+        end
       end
     end
   end
