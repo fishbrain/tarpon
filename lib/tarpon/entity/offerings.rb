6055f2369d7b15668ba3398b5ea21af8808f74cf
Sammy Henningsson <sammy.henningsson@gmail.com>
2021-01-08 13:38:56 +0100

    Pass real keyword arguments

--- lib/tarpon/entity/offerings.rb
+++ lib/tarpon/entity/offerings.rb
@@ -12,12 +12,12 @@ module Tarpon
       def initialize(current_offering_id:, offerings:, **)
         @current_offering_id = current_offering_id
         @offerings = offerings.each_with_object({}) do |offering, map|
-          map[offering[:identifier].to_sym] = Tarpon::Entity::Offering.new(offering)
+          map[offering[:identifier].to_sym] = Tarpon::Entity::Offering.new(**offering)
         end
       end
 
-      def each
-        @offerings.each { |o| yield o }
+      def each(&block)
+        @offerings.each(&block)
       end
 
       def [](identifier)
--- lib/tarpon/request/subscriber/offering.rb
+++ lib/tarpon/request/subscriber/offering.rb
@@ -14,7 +14,7 @@ module Tarpon
           response = perform(method: :get, path: path.to_s, headers: { 'x-platform': platform.to_s }, key: :public)
           return response unless response.success?
 
-          Tarpon::Entity::Offerings.new(response.raw)
+          Tarpon::Entity::Offerings.new(**response.raw)
         end
 
         private
